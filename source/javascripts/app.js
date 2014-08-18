d04ca2ab18c63ab0bdd99f3e877c76766b28c6bd
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2014-08-18 09:23:59 -0300

    first commit

---
+++ source/javascripts/app.js
@@ -0,0 +1,34 @@
+/*
+** Slide
+*/
+
+$(function () {
+    $('.alg-slide').bxSlider({
+        prevText: '<i class="glyphicon glyphicon-chevron-left"></i>',
+        nextText: '<i class="glyphicon glyphicon-chevron-right"></i>'
+    });
+});
+
+/*
+** Sidebar
+*/
+
+function sidebarToggle () {
+    $sidebar = $('#alg-sidebar');
+
+    if ($sidebar.width() == '0') {
+        $('#alg-sidebar').animate({ width: '700px' }, 200);
+        $('#alg-shadow-overlay').fadeIn(200);
+    } else {
+        $('#alg-sidebar').animate({ width: '0' }, 200);
+        $('#alg-shadow-overlay').fadeOut(200);
+    }
+}
+
+/*
+** Paralax
+*/
+
+$(function () {
+    $.stellar({ horizontalScrolling: false });
+});
